331506f841e54a433c0f7de6818a0115361ccf06
anushuyabaidya <anushuyabaidya11@gmail.com>
2024-12-01 16:34:54 -0600

    seed

--- src/evaluate_llm.py
+++ src/evaluate_llm.py
@@ -10,6 +10,10 @@ from sklearn.model_selection import train_test_split
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers import DataCollatorWithPadding
 from transformers import TrainingArguments, Trainer
+from transformers import set_seed
+
+# Set a fixed seed
+set_seed(42)
 
 
 class EvaluateLLM:
